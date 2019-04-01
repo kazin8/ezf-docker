ed42a6a3de4bed03ed64d1b4e7621349d6f94827
awful <9866494@gmail.com>
2019-04-01 19:19:17 +0300

    DB Init scripts Separate notification service

--- mysql/init_scripts/ez-notifications.sql
+++ mysql/init_scripts/ez-notifications.sql
@@ -1,3 +1,11 @@
 CREATE DATABASE ez_notifications;
 
 GRANT ALL PRIVILEGES ON ez_notifications.* TO 'ezf'@'%';
+
+CREATE USER 'ezf_test'@'%' IDENTIFIED BY 'ezf_test';
+
+CREATE DATABASE ez_notifications_test;
+
+GRANT ALL PRIVILEGES ON ez_notifications_test.* TO 'ezf_test'@'%';
+GRANT ALL PRIVILEGES ON ez_notifications_test.* TO 'ezf'@'%';
+
