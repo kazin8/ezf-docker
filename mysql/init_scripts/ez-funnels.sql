ebf70f102875560692b24ea7c6748b82929157ad
awful <9866494@gmail.com>
2019-04-24 18:48:11 +0300

    EZFunnels init environment

---
+++ mysql/init_scripts/ez-funnels.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE ez_funnels;
+
+GRANT ALL PRIVILEGES ON ez_funnels.* TO 'ezf'@'%';
+
+CREATE DATABASE ez_funnels_test;
+
+GRANT ALL PRIVILEGES ON ez_funnels_test.* TO 'ezf_test'@'%';
+GRANT ALL PRIVILEGES ON ez_funnels_test.* TO 'ezf'@'%';
+
