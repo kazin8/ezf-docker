dac5d8b18809b762cec1d90739054cdd521ec523
awful <9866494@gmail.com>
2019-04-26 15:25:23 +0300

    EZWebinar init environment

---
+++ mysql/init_scripts/ez-webinar.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE ez_webinar;
+
+GRANT ALL PRIVILEGES ON ez_webinar.* TO 'ezf'@'%';
+
+CREATE DATABASE ez_webinar_test;
+
+GRANT ALL PRIVILEGES ON ez_webinar_test.* TO 'ezf_test'@'%';
+GRANT ALL PRIVILEGES ON ez_webinar_test.* TO 'ezf'@'%';
+
