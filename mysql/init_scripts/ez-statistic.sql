ae8425c675ca8ffefe6c0d9ab29eb898c02cec07
awful <9866494@gmail.com>
2019-04-30 14:10:31 +0300

    Fix git ignore

---
+++ mysql/init_scripts/ez-statistic.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE ez_statistic;
+
+GRANT ALL PRIVILEGES ON ez_statistic.* TO 'ezf'@'%';
+
+CREATE DATABASE ez_statistic_test;
+
+GRANT ALL PRIVILEGES ON ez_statistic_test.* TO 'ezf_test'@'%';
+GRANT ALL PRIVILEGES ON ez_statistic_test.* TO 'ezf'@'%';
+
